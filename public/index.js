154af1f1c40d7602a07919d5bb40131daee6fc69
DESKTOP-O90FFR2\User <jason123jay@gmail.com>
2021-06-11 10:56:18 +0200

    vue

--- public/index.js
+++ public/index.js
@@ -1,162 +1,26 @@
-//Select DOM
-const todoInput = document.querySelector(".todo-input");
-const todoButton = document.querySelector(".todo-button");
-const todoList = document.querySelector(".todo-list");
-const filterOption = document.querySelector(".filter-todo");
-const todoTime = document.querySelector(".todo-date");
-const todoHour = document.querySelector(".todo-time");
-//Event Listeners
-document.addEventListener("DOMContentLoaded", getTodos);
-todoButton.addEventListener("click", addTodo);
-todoList.addEventListener("click", deleteTodo);
-filterOption.addEventListener("click", filterTodo);
-
-//Functions
-
-function addTodo(e) {
-  //Prevent natural behaviour
-  e.preventDefault();
-  //Create todo div
-  const todoDiv = document.createElement("div");
-  todoDiv.classList.add("todo");
-  
-  //Create list
-  const newTodo = document.createElement("li");
- // input
- const task = document.createElement("div");
- task.innerText = todoInput.value
- newTodo.appendChild(task)
- 
-
-  //add date 
-  const todoDate = document.createElement("div")
-  todoDate.innerHTML = todoTime.value;
-newTodo.appendChild(todoDate)
-
-  //add time 
-  const todoTimeClock = document.createElement("div")
-  todoTimeClock.innerHTML = todoHour.value;
-  newTodo.appendChild(todoTimeClock)
- 
-  //
-  newTodo.classList.add("todo-item");
-  todoDiv.appendChild(newTodo);
-   //Save to local - do this last
-   let newData = []
-   dataInput= JSON.stringify(todoInput.value)
-   localStorage.setItem("userInput",dataInput)
-
-   dataTime = JSON.stringify(todoTime.value)
-   localStorage.setItem("userTime",dataTime)
-     //Create Completed Button
-     
-  const completedButton = document.createElement("button");
-  completedButton.innerHTML = `<i class="fas fa-check"></i>`;
-  completedButton.classList.add("complete-btn");
-  todoDiv.appendChild(completedButton);
-  //Create trash button
-  const trashButton = document.createElement("button");
-  trashButton.innerHTML = `<i class="fas fa-trash"></i>`;
-  trashButton.classList.add("trash-btn");
-  todoDiv.appendChild(trashButton);
-  //attach final Todo
-  todoList.appendChild(todoDiv);
-}
-
-function deleteTodo(e) {
-  const item = e.target;
-
-  if (item.classList[0] === "trash-btn") {
-    // e.target.parentElement.remove();
-    const todo = item.parentElement;
-    todo.classList.add("fall");
-    //at the end
-    removeLocalTodos(todo);
-    todo.addEventListener("transitionend", e => {
-      todo.remove();
-    });
-  }
-  if (item.classList[0] === "complete-btn") {
-    const todo = item.parentElement;
-    todo.classList.toggle("completed");
-    console.log(todo);
-  }
-}
-
-function filterTodo(e) {
-  const todos = todoList.childNodes;
-  todos.forEach(function(todo) {
-    switch (e.target.value) {
-      case "all":
-        todo.style.display = "flex";
-        break;
-      case "completed":
-        if (todo.classList.contains("completed")) {
-          todo.style.display = "flex";
-        } else {
-          todo.style.display = "none";
-        }
-        break;
-      case "uncompleted":
-        if (!todo.classList.contains("completed")) {
-          todo.style.display = "flex";
-        } else {
-          todo.style.display = "none";
-        }
+const app = new Vue({
+  el: '#app',
+  data: {
+    title: 'My to do App',
+    newTodo: '',
+    todos: []
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        title: this.newTodo,
+        done: false
+      });
+      this.newTodo = '';
+    },
+    removeTodo(todo) {
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos.splice(todoIndex, 1);
+    },
+    allDone() {
+      this.todos.forEach(todo => {
+        todo.done = true;
+      });
     }
-  });
-}
-
-// // function saveLocalTodos(todo) {
-//   let todos;
-//   if (localStorage.getItem("todos") === null) {
-//     todos = [];
-//   } else {
-//     todos = JSON.parse(localStorage.getItem("todos"));
-//   }
-//   todos.push(todo);
-//   localStorage.setItem("todos", JSON.stringify(todos));
-// // }
-// // function removeLocalTodos(todo) {
-//   let todos;
-//   if (localStorage.getItem("todos") === null) {
-//     todos = [];
-//   } else {
-//     todos = JSON.parse(localStorage.getItem("todos"));
-//   }
-//   const todoIndex = todo.children[0].innerText;
-//   todos.splice(todos.indexOf(todoIndex), 1);
-//   localStorage.setItem("todos", JSON.stringify(todos));
-// }
-
-function getTodos() {
-  let todos;
-  if (localStorage.getItem("todos") === null) {
-    todos = [];
-  } else {
-    todos = JSON.parse(localStorage.getItem("todos"));
   }
-  todos.forEach(function(todo) {
-    //Create todo div
-    const todoDiv = document.createElement("div");
-    todoDiv.classList.add("todo");
-    //Create list
-    const newTodo = document.createElement("li");
-    newTodo.innerText = todo;
-    newTodo.classList.add("todo-item");
-    todoDiv.appendChild(newTodo);
-    todoInput.value = "";
-    //Create Completed Button
-    const completedButton = document.createElement("button");
-    completedButton.innerHTML = `<i class="fas fa-check"></i>`;
-    completedButton.classList.add("complete-btn");
-    todoDiv.appendChild(completedButton);
-    //Create trash button
-    const trashButton = document.createElement("button");
-    trashButton.innerHTML = `<i class="fas fa-trash"></i>`;
-    trashButton.classList.add("trash-btn");
-    todoDiv.appendChild(trashButton);
-    //attach final Todo
-    todoList.appendChild(todoDiv);
-  });
-}
+});
